542a8e295d512f71ed3de9bd59b235f9e929b789
alperenkavukk <kavuk.67.76@gmail.com>
2023-07-18 14:21:38 +0300

    Initial Commit

---
+++ Pointo/PointoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointoApp.swift
+//  Pointo
+//
+//  Created by Alperen Kavuk on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PointoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
